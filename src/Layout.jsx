66fef4b0a87cd34f8f59a2df86f2d6c511afebe7
hemant9808 <143936287+Hemant9808@users.noreply.github.com>
2025-05-20 17:17:07 +0530

    add chat bot

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -14,7 +14,7 @@ function Layout() {
       <div className="flex-1 flex flex-col">
         <Outlet />
       </div>
-      {/* {!hideFab && <FabButton />} */}
+      {!hideFab && <FabButton />}
      {!hideFab && <Footer />}
     </div>
   )
